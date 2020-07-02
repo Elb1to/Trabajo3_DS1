35ea30d084f0c77e6f807e60c999b8a3703f4a10
Elb1to <elbitomc@gmail.com>
2020-07-02 18:16:04 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Trabajo3_DS1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trabajo3_DS1.dir/main.c.obj"
+  "Trabajo3_DS1.exe"
+  "Trabajo3_DS1.exe.manifest"
+  "Trabajo3_DS1.pdb"
+  "libTrabajo3_DS1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Trabajo3_DS1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
